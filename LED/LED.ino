b67276549cfa57d5d0fb27a15e9d54922fe720c8
CodelinkZ <32823904+CodelinkZ@users.noreply.github.com>
2017-12-08 15:35:56 +0000

    Add files via upload

---
+++ LED/LED.ino
@@ -0,0 +1,28 @@
+
+
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(9,OUTPUT);
+}
+
+void loop() {
+while(true){
+  ChangeLED(true);
+  delay(1000);
+  ChangeLED(false);
+  delay(1000);
+}
+
+}
+
+void ChangeLED(boolean hasAFix){
+    if (hasAFix == true) {
+    digitalWrite(9,LOW);
+    digitalWrite(8,HIGH);
+  }
+  else {
+    digitalWrite(8,LOW);
+    digitalWrite(9,HIGH);
+  }
+}
+
